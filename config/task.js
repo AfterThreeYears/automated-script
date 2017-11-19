ab4da21069bd6726e3bd92194e2f3e7de7e7a952
wbb <4703@qq.com>
2017-11-19 15:01:31 +0800

    添加多任务

--- config/task.js
+++ config/task.js
@@ -1,11 +1,20 @@
-const ids = ['222222224793', '222222224797', '222222224798', '222222224786'];
-const cookie = {
-  name: 'accessToken',
-  value: 'QlVTSV9VU0VSX0lEX0xPR0lOXzExOQ==',
-  path: '/',
+const config = {
+  // 'pre': {
+  //   ids: ['222222224273', '222222224243'],
+  //   cookie: {
+  //     name: 'accessToken',
+  //     value: '"QlVTSV9VU0VSX0lEX0xPR0lOXzEyNA=="',
+  //   },
+  //   host: 'http://pre-partner.test.meipu.cn',
+  // },
+  'daliy': {
+    ids: ['222222224793', '222222224797'],
+    cookie: {
+      name: 'accessToken',
+      value: 'QlVTSV9VU0VSX0lEX0xPR0lOXzExOQ==',
+    },
+    host: 'http://daily.pis.meipu.cn',
+  },
 };
 
-module.exports = {
-  ids,
-  cookie,
-};
+module.exports = config;
