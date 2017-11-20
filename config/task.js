5320aa7737a6562c6ffb9e974df61d40d88fcf67
wbb <4703@qq.com>
2017-11-20 10:52:42 +0800

    隐藏信息

--- config/task.js
+++ config/task.js
@@ -1,19 +1,19 @@
 const config = {
-  // 'pre': {
-  //   ids: ['222222224273', '222222224243'],
-  //   cookie: {
-  //     name: 'accessToken',
-  //     value: '"QlVTSV9VU0VSX0lEX0xPR0lOXzEyNA=="',
-  //   },
-  //   host: 'http://pre-partner.test.meipu.cn',
-  // },
-  'daliy': {
-    ids: ['222222224793', '222222224797'],
+  'pre': {
+    ids: [],
     cookie: {
-      name: 'accessToken',
-      value: 'QlVTSV9VU0VSX0lEX0xPR0lOXzExOQ==',
+      name: '',
+      value: '',
     },
-    host: 'http://daily.pis.meipu.cn',
+    host: '',
+  },
+  'daliy': {
+    ids: ['', ''],
+    cookie: {
+      name: '',
+      value: '',
+    },
+    host: '',
   },
 };
 
